c8eb79a67769ef0d0bb8d116178a044a3bad627d
Ozan Yıldız <developer@yildizozan.com>
2016-08-18 23:12:30 +0300

    Add RegisterActivity

---
+++ app/src/main/java/com/yildizozan/yurtbasi/RegisterActivity.java
@@ -0,0 +1,16 @@
+package com.yildizozan.yurtbasi;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * Created by Ozan on 8/18/2016.
+ */
+
+public class RegisterActivity extends Activity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_register);
+    }
+}
